e358106483f5a280977eeb89cfc7a9c20f65c933
Andres Carrasquero <andresj172020@gmail.com>
2023-10-01 11:44:50 -0500

    deleted the .next folder

--- src/constants/components/commonLayout/Header/header.ts
+++ src/constants/components/commonLayout/Header/header.ts
@@ -2,29 +2,4 @@ import { HeaderButtonsType } from '../../../../typesDefs/components/commonLayout
 import { AllRoutes } from '../../../routes/routes';
 
 export const HeaderButtons: HeaderButtonsType[] = [
-  {
-    id: 'about-us',
-    link: AllRoutes.ABOUTUS,
-    name: 'pages.header.about-us'
-  },
-  {
-    id: 'my-skills',
-    link: AllRoutes.SKILLS,
-    name: 'pages.header.skills'
-  },
-  {
-    id: 'blog',
-    link: AllRoutes.BLOG,
-    name: 'pages.header.blog'
-  },
-  {
-    id: 'work-team-section',
-    link: AllRoutes.WORK_TEAM,
-    name: 'pages.header.work-team'
-  },
-  {
-    id: 'contact-section',
-    link: AllRoutes.CONTACT,
-    name: 'pages.header.contact'
-  }
 ];
