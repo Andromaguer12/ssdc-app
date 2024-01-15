cb814198c18a7bb82978e32971ea183f2eceba23
Andres Carrasquero <andresj172020@gmail.com>
2024-01-15 11:07:25 -0400

    fixes

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -20,7 +20,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
           rel="stylesheet"
         />
-        <title>SSDC</title>
+        <title>DominoElite</title>
       </Head>
       <body>
         <Main />
--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -8,6 +8,7 @@ const DashboardAdminIndex = () => {
     <>
       <Head>
         <title>DominoElite</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
       </Head>
       <Dashboard />
     </>
